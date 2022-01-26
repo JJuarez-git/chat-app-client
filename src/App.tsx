a2ce7821f3fa978e74ba60c562909831ed6f37de
Jose Carlos Juarez <josecarlosjc30@gmail.com>
2022-01-26 23:31:55 +0100

    App component modified

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import MainApp from "./pages/MainApp";
+import Login from "./pages/Login";
+import Footer from "./components/Footer";
+import {
+  BrowserRouter as Router,
+  /* Switch, */
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <div className="App">
+        {/* <MainApp /> */}
+        <Login />
+        {/* <Footer /> */}
+      </div>
+      {/* <Switch>
+        <Route>
+          
+        </Route>
+      </Switch> */}
+    </Router>
   );
 }
 
