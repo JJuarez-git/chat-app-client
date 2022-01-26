b1fdfcbc3ad056b4a6c5fe593c471132795069bf
Jose Carlos Juarez <josecarlosjc30@gmail.com>
2022-01-26 23:30:38 +0100

    Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 module.exports = {
   content: ['./src/**/*.{ts,tsx}'],
+  darkMode: 'class',
   theme: {
     fontFamily: {
-      'outfit': ['Outfit','sans-serif']
+      'outfit': ['Outfit','sans-serif'],
+      'source-sans': ['Source Sans 3', 'sans-serif']
+    },
+    maxWidth: {
+      '1/5': '20%',
+      '2/5': '40%',
+      '3/5': '60%',
+      '4/5': '80%',
     },
     extend: {},
   },
