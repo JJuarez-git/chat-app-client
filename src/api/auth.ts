002b4041cd167e248f35bedf90ed5fcca2f2716b
Jose Carlos Juarez <josecarlosjc30@gmail.com>
2022-01-28 11:35:38 +0100

    Auth API methods updated

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,7 +1,32 @@
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, sendPasswordResetEmail } from "firebase/auth";
 import { GLOBALS } from "../globals"
 import axios from "axios";
+import { firebaseApp } from '../firebase';
+import UserService from '../services/UserService';
 
-export const signIn = (authData: { username: string, password: string }) => {
-    const url = `${GLOBALS.API_URL}/auth`
-    return axios.post(url, authData, {responseType: 'json'})
+const auth = getAuth(firebaseApp)
+const userService = UserService.instance
+
+export const signIn = (email: string, password: string) => {
+    signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => userService.setEmail(userCredential.user))
+        .catch((error) => console.error(error.code, error.message))
+}
+
+export const signUp = (email: string, password: string) => {
+    createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => userService.setEmail(userCredential.user))
+        .catch((error) => console.error(error.code, error.message))
+}
+
+export const resetPassword = (email: string) => {
+    sendPasswordResetEmail(auth, email)
+        .then(() => console.log("Correo enviado"))
+        .catch((error) => console.error(error.code, error.message))
+}
+
+export const logOut = () => {
+    signOut(auth)
+        .then(() => userService.setEmail(null))
+        .catch((error) => console.error(error.code, error.message))
 }
