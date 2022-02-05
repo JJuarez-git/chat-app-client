d31db938d43d4086cc5cc91c2ede8d70fb01116b
Jose Carlos Juarez <jjuarez.dev@gmail.com>
2022-02-05 11:26:52 +0100

    New Authtentication component

---
+++ src/pages/authentication/ForgotPassword.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ForgotPassword = () => {
+   return(
+      <div>
+         <p>Has olvidao la contraseña maricon</p>
+      </div>
+   )
+}
+
+export default ForgotPassword
