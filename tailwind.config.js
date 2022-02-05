859ed18dc6414e9cf3d65de38035e98e54060325
Jose Carlos Juarez <jjuarez.dev@gmail.com>
2022-02-05 11:21:10 +0100

    Update Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,21 @@
 module.exports = {
-  content: ['./src/**/*.{ts,tsx}'],
-  darkMode: 'class',
-  theme: {
-    fontFamily: {
-      'outfit': ['Outfit','sans-serif'],
-      'source-sans': ['Source Sans 3', 'sans-serif']
-    },
-    maxWidth: {
-      '1/5': '20%',
-      '2/5': '40%',
-      '3/5': '60%',
-      '4/5': '80%',
-    },
-    extend: {},
-  },
-  plugins: [],
+   content: ['./src/**/*.{ts,tsx}'],
+   darkMode: 'class',
+   theme: {
+      /* colors: {
+         'modalBackdrop':'#0006'
+      }, */
+      fontFamily: {
+         'outfit': ['Outfit', 'sans-serif'],
+         'source-sans': ['Source Sans 3', 'sans-serif']
+      },
+      maxWidth: {
+         '1/5': '20%',
+         '2/5': '40%',
+         '3/5': '60%',
+         '4/5': '80%',
+      },
+      extend: {},
+   },
+   plugins: [],
 }
